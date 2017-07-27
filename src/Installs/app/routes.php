a8d3ad41cdb8d5fe0fb876e231ad1c5f3cf91b2e
Nicolas Brabant <nbrabant@itm.fr>
2017-07-27 17:26:46 +0200

    Add locale routes

--- src/Installs/app/routes.php
+++ src/Installs/app/routes.php
@@ -1,4 +1,25 @@
 
-/* ================== Homepage + Admin Routes ================== */
+/* ================== Homepage + Admin Routes + Localisation ================== */
 
 require __DIR__.'/admin_routes.php';
+
+// Localization
+Route::get('/js/lang.js', function () {
+    $strings = Cache::rememberForever('lang.js', function () {
+        $lang = config('app.locale');
+
+        $files   = glob(resource_path('lang/' . $lang . '/*.php'));
+        $strings = [];
+
+        foreach ($files as $file) {
+            $name           = basename($file, '.php');
+            $strings[$name] = require $file;
+        }
+
+        return $strings;
+    });
+
+    header('Content-Type: text/javascript');
+    echo('window.i18n = ' . json_encode($strings) . ';');
+    exit();
+})->name('assets.lang');
