74efe7d032c8735bcbd80746557133282fab24dd
nbrabant <brabantnicolas59@gmail.com>
2017-08-13 15:51:21 +0200

    dynamic lang file copy

--- src/LATranslate.php
+++ src/LATranslate.php
@@ -18,6 +18,7 @@ class LATranslate
 	    if(is_null(self::$_instance)) {
 	        self::$_instance = new LATranslate();
 	    }
+
 	    return self::$_instance;
 	}
 
@@ -26,29 +27,19 @@ class LATranslate
 	}
 
 	public function copyTranslations($from, $to) {
+		if (!is_dir($from . '/resources/lang') || empty($list = \File::directories($from . '/resources/lang'))) {
+			return false;
+		}
 
-		// @TODO : make this code dynamic :
-		// list lang resources
-		// If file exists, add only the new localisation lines
-		// overelse, copy them on the directory
-		$this->copyFile($from . "/resources/lang/fr.json", 				$to . "/resources/lang/fr.json");
-		$this->copyFile($from . "/resources/lang/fr/global.php", 		$to . "/resources/lang/fr/global.php");
-		$this->copyFile($from . "/resources/lang/fr/auth.php",   		$to . "/resources/lang/fr/auth.php");
-		$this->copyFile($from . "/resources/lang/fr/errors.php", 		$to . "/resources/lang/fr/errors.php");
-		$this->copyFile($from . "/resources/lang/fr/emails.php", 		$to . "/resources/lang/fr/emails.php");
-		$this->copyFile($from . "/resources/lang/fr/pagination.php", 	$to . "/resources/lang/fr/pagination.php");
-
-		$this->copyFile($from . "/resources/lang/en.json", 				$to . "/resources/lang/en.json");
-		$this->copyFile($from . "/resources/lang/en/global.php", 		$to . "/resources/lang/en/global.php");
-		$this->copyFile($from . "/resources/lang/en/auth.php",   		$to . "/resources/lang/en/auth.php");
-		$this->copyFile($from . "/resources/lang/en/errors.php", 		$to . "/resources/lang/en/errors.php");
-		$this->copyFile($from . "/resources/lang/en/emails.php", 		$to . "/resources/lang/en/emails.php");
-		$this->copyFile($from . "/resources/lang/en/pagination.php", 	$to . "/resources/lang/en/pagination.php");
-		// end todo
-
+		foreach ($list as $directory) {
+			foreach (\File::files($directory) as $file) {
+				$this->copyFile($file, str_replace($from, $to, $file);
+			}
+		}
 	}
 
-	public static function mergeTranslations() {
+	public function mergeTranslations() {
+
 	}
 
 
